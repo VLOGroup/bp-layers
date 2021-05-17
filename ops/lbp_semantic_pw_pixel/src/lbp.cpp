226d67f73396dfd14f1cbb12445e542781256260
Patrick Knöbelreiter <knoebelreiter@icg.tugraz.at>
2021-05-17 09:05:39 +0200

    Add global license file

--- ops/lbp_semantic_pw_pixel/src/lbp.cpp
+++ ops/lbp_semantic_pw_pixel/src/lbp.cpp
@@ -1,22 +1,3 @@
-// This file is part of bp-layers.
-//
-// Copyright (C) 2020 Patrick Knöbelreiter <knoebelreiter at icg dot tugraz dot at>
-// Christian Sormann <christian dot sormann at icg dot tugraz dot at>
-// Institute for Computer Graphics and Vision, Graz University of Technology
-// https://www.tugraz.at/institute/icg/teams/team-pock/
-//
-// bp-layers is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Affero General Public License as published by the Free Software
-// Foundation, either version 3 of the License, or any later version.
-//
-// bp-layers is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 #include <torch/extension.h>
 #include <iostream>
 
